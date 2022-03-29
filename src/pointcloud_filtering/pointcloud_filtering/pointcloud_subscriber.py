33576530a65449a2408599ceaf5a2e6bbde86b11
gasparyanartur <gasparyanartur99@gmail.com>
2022-03-29 14:16:35 +0200

    Add bigg

--- src/pointcloud_filtering/pointcloud_filtering/pointcloud_subscriber.py
+++ src/pointcloud_filtering/pointcloud_filtering/pointcloud_subscriber.py
@@ -1,10 +1,3 @@
-from sensor_msgs.msg import PointCloud2, PointField
-import struct
-import math
-import ctypes
-from collections import namedtuple
-import sys
-import os
 from custom.msg import LidarMessage
 
 import rclpy
@@ -16,6 +9,8 @@ from probreg import cpd
 
 import numpy as np
 import open3d as o3d
+
+from . import dangerzone
 
 
 class PCDListener(Node):
@@ -41,12 +36,12 @@ class PCDListener(Node):
         # the ROS1 package.
         # https://github.com/ros/common_msgs/blob/noetic-devel/sensor_msgs/src/sensor_msgs/point_cloud2.py
 
-        pcd_as_numpy_array = np.array(list(read_points(msg.pcl_response)))
+        pcd_as_numpy_array = np.array(
+            list(dangerzone.read_points(msg.pcl_response)))
         dist = 1
         filtered = np.array(
             [row for row in pcd_as_numpy_array if row[0]**2 + row[1]**2 + row[2]**2 < dist**2])
 
-        # The rest here is for visualization.
         self.o3d_pcd = o3d.geometry.PointCloud(
             o3d.utility.Vector3dVector(filtered))
 
@@ -107,99 +102,9 @@ class PCDListener(Node):
             print(np.array(box.extent))
             if dx-errorxd < box.extent[0] < dx + errorxu and dy-erroryd < box.extent[1] < dy + erroryu and dz-errorzd < box.extent[2] < dz + errorzu and volume - verror < box.volume() < volume + verror:
                 blue_boxes.append(box)
+
         o3d.visualization.draw_geometries(
             [self.o3d_pcd, mesh_frame] + blue_boxes)
-
-
-# The code below is "ported" from
-# https://github.com/ros/common_msgs/tree/noetic-devel/sensor_msgs/src/sensor_msgs
-# I'll make an official port and PR to this repo later:
-# https://github.com/ros2/common_interfaces
-
-_DATATYPES = {}
-_DATATYPES[PointField.INT8] = ('b', 1)
-_DATATYPES[PointField.UINT8] = ('B', 1)
-_DATATYPES[PointField.INT16] = ('h', 2)
-_DATATYPES[PointField.UINT16] = ('H', 2)
-_DATATYPES[PointField.INT32] = ('i', 4)
-_DATATYPES[PointField.UINT32] = ('I', 4)
-_DATATYPES[PointField.FLOAT32] = ('f', 4)
-_DATATYPES[PointField.FLOAT64] = ('d', 8)
-
-
-def read_points(cloud, field_names=None, skip_nans=False, uvs=[]):
-    """
-    Read points from a L{sensor_msgs.PointCloud2} message.
-    @param cloud: The point cloud to read from.
-    @type  cloud: L{sensor_msgs.PointCloud2}
-    @param field_names: The names of fields to read. If None, read all fields. [default: None]
-    @type  field_names: iterable
-    @param skip_nans: If True, then don't return any point with a NaN value.
-    @type  skip_nans: bool [default: False]
-    @param uvs: If specified, then only return the points at the given coordinates. [default: empty list]
-    @type  uvs: iterable
-    @return: Generator which yields a list of values for each point.
-    @rtype:  generator
-    """
-    assert isinstance(
-        cloud, PointCloud2), 'cloud is not a sensor_msgs.msg.PointCloud2'
-    fmt = _get_struct_fmt(cloud.is_bigendian, cloud.fields, field_names)
-    width, height, point_step, row_step, data, isnan = cloud.width, cloud.height, cloud.point_step, cloud.row_step, cloud.data, math.isnan
-    unpack_from = struct.Struct(fmt).unpack_from
-
-    if skip_nans:
-        if uvs:
-            for u, v in uvs:
-                p = unpack_from(data, (row_step * v) + (point_step * u))
-                has_nan = False
-                for pv in p:
-                    if isnan(pv):
-                        has_nan = True
-                        break
-                if not has_nan:
-                    yield p
-        else:
-            for v in range(height):
-                offset = row_step * v
-                for u in range(width):
-                    p = unpack_from(data, offset)
-                    has_nan = False
-                    for pv in p:
-                        if isnan(pv):
-                            has_nan = True
-                            break
-                    if not has_nan:
-                        yield p
-                    offset += point_step
-    else:
-        if uvs:
-            for u, v in uvs:
-                yield unpack_from(data, (row_step * v) + (point_step * u))
-        else:
-            for v in range(height):
-                offset = row_step * v
-                for u in range(width):
-                    yield unpack_from(data, offset)
-                    offset += point_step
-
-
-def _get_struct_fmt(is_bigendian, fields, field_names=None):
-    fmt = '>' if is_bigendian else '<'
-
-    offset = 0
-    for field in (f for f in sorted(fields, key=lambda f: f.offset) if field_names is None or f.name in field_names):
-        if offset < field.offset:
-            fmt += 'x' * (field.offset - offset)
-            offset = field.offset
-        if field.datatype not in _DATATYPES:
-            print(
-                'Skipping unknown PointField datatype [%d]' % field.datatype, file=sys.stderr)
-        else:
-            datatype_fmt, datatype_length = _DATATYPES[field.datatype]
-            fmt += field.count * datatype_fmt
-            offset += field.count * datatype_length
-
-    return fmt
 
 
 def main(args=None):
