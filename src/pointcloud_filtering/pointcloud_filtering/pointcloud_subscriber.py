98123313864436022513ad9a2faf6af16757f663
gasparyanartur <gasparyanartur99@gmail.com>
2022-04-08 15:34:26 +0200

    update pointcloud subscriber

--- src/pointcloud_filtering/pointcloud_filtering/pointcloud_subscriber.py
+++ src/pointcloud_filtering/pointcloud_filtering/pointcloud_subscriber.py
@@ -6,6 +6,8 @@ import sensor_msgs.msg as sensor_msgs
 import matplotlib.pyplot as plt
 from probreg import cpd
 
+import copy
+
 
 import numpy as np
 import open3d as o3d
@@ -45,7 +47,7 @@ def get_box_model():
         raise FileNotFoundError("Could not file box file. Make sure the program is run from workspace")               
 
     box = o3d.io.read_point_cloud(box_path_full)
-    box = box.random_down_sample(sampling_ratio=2000/len(np.asarray(box.points)))
+    box = box.random_down_sample(sampling_ratio=1500/len(np.asarray(box.points)))
 
     return box
 
@@ -74,7 +76,21 @@ def process_clusters(pcd):
     return max_label, labels, clusters, bounding_boxes
 
 
-def find_blue_box(bounding_boxes):
+def find_largest_cluster(bounding_boxes):
+    largest_volume = 0
+    largest_index = -1
+
+    for index, box in enumerate(bounding_boxes):
+        volume = box.volume()
+
+        if volume > largest_volume:
+            largest_index, largest_volume = index, volume
+
+    return largest_index, bounding_boxes[largest_index]
+
+
+
+def find_blue_box_old(bounding_boxes):
     bounds = np.array([0.46235329, 0.36464842, 0.18794718])
     lower_margin = np.array([0.05, 0.05, 0.05])
     upper_margin = np.array([0.15, 0.15, 0.15])
@@ -126,56 +142,91 @@ def visualize_scene(pcd, max_label, labels, blue_box=None):
         o3d.visualization.draw_geometries(geometries)
 
 
+def get_cloud(msg, use_example=False, store_example=False):
+    o3d_pcd = None    
+    path = "./src/pointcloud_filtering/pointcloud_filtering/example_box_cloud.pcd"
+
+    if use_example:
+        try:
+            o3d_pcd = o3d.io.read_point_cloud(path)
+        except:
+            print("Failed to read example.")
+
+    else:
+        pcd = np.array(list(dangerzone.read_points(msg.pcl_response)))
+        pcd = filter_pointcloud(pcd, max_distance=1.5)
+        o3d_pcd = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(pcd[:, :3]))
+
+        if store_example:
+            o3d.io.write_point_cloud(path, o3d_pcd)
+            
+    return o3d_pcd
+
+
 class PCDListener(Node):
 
     def __init__(self):
         super().__init__('pcd_subsriber_node')
 
-        # This is for visualization of the received point cloud.
         self.o3d_pcd = o3d.geometry.PointCloud()
-
-        # Set up a subscription to the 'pcd' topic with a callback to the
-        # function `listener_callback`
-        self.pcd_subscriber = self.create_subscription(
-            LidarMessage,    # Msg type
-            'pcl',                      # topic
-            self.listener_callback,      # Function to call
-            10                          # QoS
-        )
+        self.pcd_subscriber = self.create_subscription(LidarMessage, 'pcl', self.listener_callback, 10)
 
     def listener_callback(self, msg):
-        # Here we convert the 'msg', which is of the type PointCloud2.
-        # I ported the function read_points2 from
-        # the ROS1 package.
         # https://github.com/ros/common_msgs/blob/noetic-devel/sensor_msgs/src/sensor_msgs/point_cloud2.py
 
-        pcd = np.array(list(dangerzone.read_points(msg.pcl_response)))
-        pcd = filter_pointcloud(pcd, max_distance=1.5)
+        #pcd = np.array(list(dangerzone.read_points(msg.pcl_response)))
+        #pcd = filter_pointcloud(pcd, max_distance=1.5)
+        #o3d_pcd = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(pcd[:, :3]))
 
-        self.o3d_pcd = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(pcd[:, :3]))
+        o3d_pcd = get_cloud(msg, store_example=True)
+
+        o3d.visualization.draw_geometries([o3d_pcd]) 
+
+        slim_pcd = o3d_pcd.voxel_down_sample(voxel_size=0.03)
+        o3d.visualization.draw_geometries([slim_pcd]) 
 
         #plane_model, inliers = self.o3d_pcd.segment_plane(distance_threshold=0.02, ransac_n=3, num_iterations=1000)
         #self.o3d_pcd = self.o3d_pcd.select_by_index(inliers, invert=True)
-
         
-        max_label, labels, clusters, bounding_boxes= process_clusters(self.o3d_pcd)
-        blue_box_index, bound_blue_box = find_blue_box(bounding_boxes)
+        max_label, labels, clusters, bounding_boxes= process_clusters(o3d_pcd)
+        blue_box_index, bound_blue_box = find_largest_cluster(bounding_boxes)
 
+        #slim_pcd = self.o3d_pcd.voxel_down_sample(voxel_size=0.05)
 
-        visualize_scene(self.o3d_pcd, max_label, labels, bound_blue_box)
+        visualize_scene(o3d_pcd, max_label, labels, bound_blue_box)
 
         if bound_blue_box is None:
             return
 
         blue_box_cluster = clusters[blue_box_index]
+        blue_box_model = get_box_model()
+        o3d.visualization.draw_geometries([blue_box_cluster])
+        o3d.visualization.draw_geometries([blue_box_model])
 
+        # TODO Perform Coherent point drift on blue box and bo model
         # https://pypi.org/project/probreg/
         print("Starting coherent point drift...")
         # TODO Fix Coherent point drift and remove max_iter
-        cpd_reg = cpd.registration_cpd(source=self.o3d_pcd, target=blue_box_cluster, maxiter=5)
-        print(cpd_reg)
-        print("Coherent point drift finished")
-        # TODO Perform Coherent point drift on blue box and bo model
+        try:
+            tf_param, _, _ = cpd.registration_cpd(source=blue_box_model, target=blue_box_cluster, maxiter=40)
+            print("Coherent point drift finished")
+        except:
+            print("Failed to do coherent point drift")
+            return
+
+        fit_pcd = copy.deepcopy(blue_box_cluster)
+        fit_pcd.points = tf_param.transform(fit_pcd.points) # Todo see if necessary
+
+        blue_box_cluster.paint_uniform_color([1, 0, 0])
+        fit_pcd.paint_uniform_color([0, 1, 0])
+
+        o3d.visualization.draw_geometries([blue_box_cluster])
+        o3d.visualization.draw_geometries([fit_pcd])
+
+        print("Returning pointcloud")
+        
+        # TODO Add response
+
 
 
 def main(args=None):
